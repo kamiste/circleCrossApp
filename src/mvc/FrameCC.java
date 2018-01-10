a975055b9e799a3b003bc9b909bb6a68b9d8e409
Kamil Stencel <stencelkamil@o2.pl>
2018-01-10 12:36:52 +0100

    change language PL to ENG, checkResult added draw, actionPerformed exclude repetition

--- src/mvc/FrameCC.java
+++ src/mvc/FrameCC.java
@@ -5,21 +5,21 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FrameCC extends JFrame implements ActionListener{
+public class FrameCC extends JFrame implements ActionListener {
 
-    private JPanel centerPanel,buttonPanel;
+    private JPanel centerPanel, buttonPanel;
     private JButton newGame;
     private JButton[] buttonT = new JButton[9];
     private JLabel topLabel;
-    private String whoPlays="O";
+    private String whoPlays = "O";
 
-    public FrameCC(){
+    public FrameCC() {
         // ustawienia okna
-        super("Gra kółko-krzyżyk"); // nazwa okna
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // wyłączanie krzyżykiem
+        super("Game circle-cross"); // nazwa okna
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // wyłączanie krzyżykiem
         setVisible(true); // widocznosc
         setLocationRelativeTo(null); // wyswietla sie na srodku ekranu
-        setSize(400,300); // wielkosc okna
+        setSize(400, 300); // wielkosc okna
         setLayout(new FlowLayout()); // glowny layout
 
         // Panel pomocniczy do wysrodkowania wszystkiego
@@ -28,7 +28,7 @@ public class FrameCC extends JFrame implements ActionListener{
 
         // ktory gracz?
 
-        topLabel = new JLabel("Teraz tura: "+whoPlays,SwingConstants.CENTER);
+        topLabel = new JLabel("Now round: " + whoPlays, SwingConstants.CENTER);
 
         // tworze panel z przyciskami
         buttonPanel = new JPanel();
@@ -36,8 +36,8 @@ public class FrameCC extends JFrame implements ActionListener{
         buttonPanel.setPreferredSize(new Dimension(210, 210));
 
         // tworze przyciski, sluchaczy i dodaje do paneluPrzyciskow
-        for(int i=0;i<9;i++){
-            buttonT[i]= new JButton(" "+i);
+        for (int i = 0; i < 9; i++) {
+            buttonT[i] = new JButton("");
             buttonT[i].addActionListener(this);
             buttonPanel.add(buttonT[i]);
         }
@@ -57,81 +57,90 @@ public class FrameCC extends JFrame implements ActionListener{
     }
 
     // zmiana gracza X / O
-    private void change(){
-        if(whoPlays.equals("O")){
-            whoPlays="X";
-            topLabel.setText("Teraz tura: "+whoPlays);
-        }
-        else {
+    private void change() {
+        if (whoPlays.equals("O")) {
+            whoPlays = "X";
+            topLabel.setText("Now round: " + whoPlays);
+        } else {
             whoPlays = "O";
-            topLabel.setText("Teraz tura: " + whoPlays);
+            topLabel.setText("Now round: " + whoPlays);
         }
     }
 
     // reset gry
-    private void resetCC(){
+    private void resetCC() {
         // for each z listy przyciskow
-        for(JButton x:buttonT){
+        for (JButton x : buttonT) {
             x.setText("");
         }
 
     }
 
-    private void checkResult(){
+    private void checkResult() {
 
-        String x = "X",o = "O";
-
-        if(
+        String x = "X", o = "O";
+        int k = 0;
+        if (
             // poziom
-            x.equals(buttonT[0].getText()) && x.equals(buttonT[1].getText()) && x.equals(buttonT[2].getText()) ||
-            x.equals(buttonT[3].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[5].getText()) ||
-            x.equals(buttonT[6].getText()) && x.equals(buttonT[7].getText()) && x.equals(buttonT[8].getText()) ||
-            // pion
-            x.equals(buttonT[0].getText()) && x.equals(buttonT[3].getText()) && x.equals(buttonT[6].getText()) ||
-            x.equals(buttonT[1].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[7].getText()) ||
-            x.equals(buttonT[2].getText()) && x.equals(buttonT[5].getText()) && x.equals(buttonT[8].getText()) ||
-            // ukos
-            x.equals(buttonT[0].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[8].getText()) ||
-            x.equals(buttonT[2].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[6].getText()))
-        {
-            JOptionPane.showMessageDialog(null,"Wygrał "+x);
+                x.equals(buttonT[0].getText()) && x.equals(buttonT[1].getText()) && x.equals(buttonT[2].getText()) ||
+                        x.equals(buttonT[3].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[5].getText()) ||
+                        x.equals(buttonT[6].getText()) && x.equals(buttonT[7].getText()) && x.equals(buttonT[8].getText()) ||
+                        // pion
+                        x.equals(buttonT[0].getText()) && x.equals(buttonT[3].getText()) && x.equals(buttonT[6].getText()) ||
+                        x.equals(buttonT[1].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[7].getText()) ||
+                        x.equals(buttonT[2].getText()) && x.equals(buttonT[5].getText()) && x.equals(buttonT[8].getText()) ||
+                        // ukos
+                        x.equals(buttonT[0].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[8].getText()) ||
+                        x.equals(buttonT[2].getText()) && x.equals(buttonT[4].getText()) && x.equals(buttonT[6].getText())) {
+            JOptionPane.showMessageDialog(null, "Won " + x);
             resetCC();
             change();
-        }
-
-        else if(
-                // poziom
+        } else if (
+            // poziom
                 o.equals(buttonT[0].getText()) && o.equals(buttonT[1].getText()) && o.equals(buttonT[2].getText()) ||
-                o.equals(buttonT[3].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[5].getText()) ||
-                o.equals(buttonT[6].getText()) && o.equals(buttonT[7].getText()) && o.equals(buttonT[8].getText()) ||
-                // pion
-                o.equals(buttonT[0].getText()) && o.equals(buttonT[3].getText()) && o.equals(buttonT[6].getText()) ||
-                o.equals(buttonT[1].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[7].getText()) ||
-                o.equals(buttonT[2].getText()) && o.equals(buttonT[5].getText()) && o.equals(buttonT[8].getText()) ||
-                // ukos
-                o.equals(buttonT[0].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[8].getText()) ||
-                o.equals(buttonT[2].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[6].getText()))
-        {
-            JOptionPane.showMessageDialog(null,"Wygrał "+o);
+                        o.equals(buttonT[3].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[5].getText()) ||
+                        o.equals(buttonT[6].getText()) && o.equals(buttonT[7].getText()) && o.equals(buttonT[8].getText()) ||
+                        // pion
+                        o.equals(buttonT[0].getText()) && o.equals(buttonT[3].getText()) && o.equals(buttonT[6].getText()) ||
+                        o.equals(buttonT[1].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[7].getText()) ||
+                        o.equals(buttonT[2].getText()) && o.equals(buttonT[5].getText()) && o.equals(buttonT[8].getText()) ||
+                        // ukos
+                        o.equals(buttonT[0].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[8].getText()) ||
+                        o.equals(buttonT[2].getText()) && o.equals(buttonT[4].getText()) && o.equals(buttonT[6].getText())) {
+            JOptionPane.showMessageDialog(null, "Won " + o);
             resetCC();
             change();
         }
 
+        // remis
+        for (JButton j : buttonT) {
+            if (j.getText().equals("X") || j.getText().equals("O")) {
+                k++;
+            }
+            if (k == 9) {
+                JOptionPane.showMessageDialog(null, "Draw!");
+                resetCC();
+                change();
+                k = 0;
+            }
+        }
     }
 
     public void actionPerformed(ActionEvent e) { // obsluga zdarzen
 
-        JButton button = (JButton)(e.getSource());
+        JButton button = (JButton) (e.getSource());
         if (button.equals(newGame)) {
             resetCC();
             change();
+        } else if(button.getText().equals("O") || button.getText().equals("X")){
+            JOptionPane.showMessageDialog(null, "Can't do that!");
         }
-        else
+         else if(button.getText().isEmpty()) {
             button.setText(whoPlays);
             change();
+        }
+            checkResult();
 
-        checkResult();
     }
-
 
 }
